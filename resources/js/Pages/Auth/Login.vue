1feaab9bed7cdf7e30d4937b0fc87b95712139dc
developer-tamim <tawsiftamim24@gmail.com>
2025-02-26 19:56:45 +0600

    login js fix

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -76,6 +76,13 @@ const submit = () => {
 </template>
 
 <script>
+import '../../assets/vendor/libs/jquery/jquery.js';
+import '../../assets/vendor/libs/popper/popper.js';
+import '../../assets/vendor/js/bootstrap.js';
+import '../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js';
+import '../../assets/vendor/js/menu.js';
+// import '../../assets/js/main.js';
+import '../../assets/vendor/libs/jquery/jquery.js';
 </script>
 
 <style>
