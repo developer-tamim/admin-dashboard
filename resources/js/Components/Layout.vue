744951a84f1bcd7ee1f067b6ea5577f9ed5b330b
developer-tamim <tawsiftamim24@gmail.com>
2025-02-28 16:41:32 +0600

    dashboard and logout fix

---
+++ resources/js/Components/Layout.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="layout-wrapper layout-content-navbar">
+      <div class="layout-container">
+        <Sidebar />
+        <div class="layout-page">
+          <Navbar />
+          <div class="content-wrapper">
+            <slot />
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+
+  <script>
+  import Sidebar from './Sidebar.vue';
+  import Navbar from './Navbar.vue';
+
+  export default {
+    components: {
+      Sidebar,
+      Navbar,
+    },
+  };
+  </script>
